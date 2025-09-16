1b3d68622c87942a3b9d5f597b4ef62b0779096b
Waleed-khattak <khattak4422004@gmail.com>
2025-09-16 13:47:57 +0500

    Removed ILMIMARKAZ folder from repo but kept locally

--- IlmiMarkaz/ILMIMARKAZ/src/index.js
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import ReactDOM from 'react-dom/client';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
