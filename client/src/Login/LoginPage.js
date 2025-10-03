0902e0d456f588b673c4045908f923ff8b3b86c3
Waleed Khattak <128230863+Waleed-khattak@users.noreply.github.com>
2025-10-03 18:33:32 +0500

    Update LoginPage.js

--- client/src/Login/LoginPage.js
+++ client/src/Login/LoginPage.js
@@ -40,7 +40,7 @@ export default function LoginPage() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://internrship-group-project-smartdesk.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ identifier: email, password, role }),
